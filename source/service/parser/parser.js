fe53106cca1bc802497188858e6a3f98e19167e1
Artem <stich6269@yandex.ru>
2016-03-24 12:59:48 +0200

    Update parser.js
    - Ended parse lessons attributes in parser service

--- source/service/parser/parser.js
+++ source/service/parser/parser.js
@@ -37,32 +37,59 @@ RAD.service("service.parser", RAD.Blanks.Service.extend({
     },
     getGroupSchedule: function(data){
         var scheduleArr = [],
+            self = this,
             $page = $(data.extras.html),
             cb = data.success,
             $content = $page.find('.contentpaneopen'),
             $table = $content.find('table'),
-            $tr = $table.find('tr');
+            $tr = $table.find('tr'),
+            currentRows;
 
-        $tr.each(function(roeCounter, item){
-            //console.log(i + ' week', $(item).html());
+        $tr.each(function(rowCounter, item){
+            currentRows =  $(item).children()
 
-            if(roeCounter == 0){
-                $(item).children().each(function(collCounter, coll){
+            if(rowCounter == 0){
+                currentRows.each(function(collCounter, col){
                     if(collCounter){
-                        //console.log(roeCounter + ' row ' + collCounter + ' coll', $(coll).html());
                         scheduleArr.push({
-                            date: $(coll).html()
+                            date: $(col).html()
                         });
                     }
                 });
-                console.log(scheduleArr);
-            }else if(roeCounter != 1){
-                
-            }
+            }else if(rowCounter != 1){
+                for (var i = 1; i <  currentRows.length; i = i+2) {
+                    var currentNumber =  Math.round(i/2)- 1,
+                        lessons = scheduleArr[currentNumber],
+                        lessonsStr = $(currentRows[i]).html(),
+                        auditoryAttr =  $(currentRows[i+1]).html().split('*'),
+                        auditory = +auditoryAttr[0],
+                        building = +auditoryAttr[1] || (auditory && 1),
+                        lessonsAttrs = self.parseLessonsAttr(lessonsStr);
 
+                    lessons.auditory = auditory;
+                    lessons.building = building;
+                    _.extend(lessons, lessonsAttrs);
+                }
+            }
         });
 
+        console.log(scheduleArr);
         //typeof cb == 'function' && cb($tr);
+    },
+    parseLessonsAttr: function(lessonsStr){
+        var begin = lessonsStr.indexOf('('),
+            subStr = lessonsStr.substr(begin),
+            end = subStr.indexOf(')'),
+            type = subStr.substr(1, end-1),
+            teacher = lessonsStr.substr(lessonsStr.lastIndexOf('<br>')+4),
+            subject = lessonsStr.substr(0, lessonsStr.indexOf('<br>'));
+
+        return{
+            type: type,
+            teacher: teacher,
+            subject: subject
+        }
+
     },
     parseCourse: function(groupName){
         var begin = groupName.indexOf('-'),
