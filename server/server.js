4b57fe3d3b13ff26410ee1ab027efb04f32de461
Artem Polishchuk <a.polishchuk@mobidev.biz>
2016-04-08 20:02:52 +0300

    - geted schedule for one group (need parser function refactoring)

--- server/server.js
+++ server/server.js
@@ -1,50 +1,42 @@
-var Crawler = require("crawler"),
-    url = require('url'),
+var siteUrl = 'http://www.model.poltava.ua/index.php?option=com_contact&view=contact&id=1&Itemid=220',
+    Crawler = require("crawler"),
     http = require('http'),
+    parser = require('./services/parser/index'),
+    _ = require('underscore'),
+    deferred = require('deferred'),
     Entities =  require('html-entities').AllHtmlEntities,
     entities = new Entities();
 
 
 
-var c = new Crawler({
-    maxConnections : 10,
+var getGroups = new Crawler({
+    maxConnections : 1,
     callback : function (error, result, $) {
-        $('.sub').each(function (index, item) {
-            console.log(entities.decode($(item).html()));
-        });
-        
+        var def = parser.getGroups($);
+        console.log('End ger group arr...');
+        def.done(function (result) {
+            console.log('Start get schedule...');
+            getSchedule(result);
+        })
     }
 });
 
+console.log('Start ger group arr...');
+getGroups.queue(siteUrl);
 
-c.queue('http://www.model.poltava.ua/index.php?option=com_contact&view=contact&id=1&Itemid=220');
+
+function getSchedule(lessonsCollection) {
+    var linkArr = _.pluck(lessonsCollection, 'link');
+    var counter = 0;
+    var getGroups = new Crawler({
+        maxConnections : 1,
+        callback : function (error, result, $) {
+            console.log(lessonsCollection[counter].name);
+            parser.getGroupSchedule($);
+            counter++;
+        }
+    });
 
 
-
-/*
-// Queue a list of URLs 
-c.queue(['http://jamendo.com/','http://tedxparis.com']);
-
-// Queue URLs with custom callbacks & parameters 
-c.queue([{
-    uri: 'http://parishackers.org/',
-    jQuery: false,
-
-    // The global callback won't be called 
-    callback: function (error, result) {
-        console.log('Grabbed', result.body.length, 'bytes');
-    }
-}]);
-
-// Queue using a function 
-var googleSearch = function(search) {
-    return 'http://www.google.fr/search?q=' + search;
-};
-c.queue({
-    uri: googleSearch('cheese')
-});
-
-// Queue some HTML code directly without grabbing (mostly for tests) 
-c.queue([{
-    html: '<p>This is a <strong>test</strong></p>'
-}]);*/
+    getGroups.queue(linkArr[0]);
+}
