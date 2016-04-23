12a668469eb2bbfbfb9d29a6a9882f192f009be6
Artem Polishchuk (Kharkiv) <a.polishchuk@mobidev.biz>
2016-04-23 20:54:15 +0300

    - go ahead, lets make callback hell

--- server/server.js
+++ server/server.js
@@ -2,18 +2,23 @@
 var pageGrabber = require('./services/spider/spider');
 var db = require('./services/mongodb/mongodb');
 
+db.dropCollections(function () {
+    console.log('Collection dropped');
+    pageGrabber.once('got-links', function (result) {
+        db.saveCollection(result.teachers, function () {
+            db.saveCollection(result.groups, function () {
+                console.log('Collections saved');
+                //pageGrabber.getLessons(result.groups);
+            })
+        });
+    });
 
-pageGrabber.on('got-links', function (result) {
-    console.log('Done parse link');
-    db.dropCollections();
-    //pageGrabber.getLessons(result.groups);
+
+    pageGrabber.getScheduleLinks();
 });
 
-db.on('db:initialized', function () {
-    console.log('db ready')
-});
 
 
 
 
-pageGrabber.getScheduleLinks();
+
--- server/services/mongodb/mongodb.js
+++ server/services/mongodb/mongodb.js
@@ -10,68 +10,51 @@ util.inherits(DatabaseDriver, EventEmitter);
 function DatabaseDriver() {
     this.dbName = 'VPU7Schedule';
     this.dbLink = 'mongodb://localhost/' + this.dbName;
-    this.debugMode = true;
+    this.debugMode = false;
     this.native = mongoose.connection;
     this.collections = [];
     this.initialize();
 }
 
+//Initialize module
 DatabaseDriver.prototype.initialize = function () {
     var self = this;
 
     EventEmitter.call(this);
     mongoose.connect(this.dbLink);
     mongoose.set('debug', this.debugMode);
-
-    this.native.on('open', function () {
-        self.collections = Object.keys(self.native.collections);
-
+    
+    this.native.once('open', function () {
+        self.dropCollections()
     })
-
 };
 
-
-
-
-
-
-
-
-
-DatabaseDriver.prototype.dropCollections = function () {
+//Drop collections
+DatabaseDriver.prototype.dropCollections = function (cb) {
     var self = this;
-
-    if(this.collections.length){
-        async.eachSeries(this.collections, function (item, callback) {
+    
+    self.collections = Object.keys(self.native.collections);
+    if(self.collections.length){
+        async.eachSeries(self.collections, function (item, callback) {
             self.native.collections[item].drop(callback);
         }, function () {
-            self.emit('db:initialized');
+            typeof cb == 'function' && cb()
         });
     }
 };
 
-
-
-
-
-
-
-
-
-
-DatabaseDriver.prototype.saveCollection = function (data) {
+//Save collection
+DatabaseDriver.prototype.saveCollection = function (data, cb) {
     if(data.length){
         async.eachSeries(data, function (item, callback) {
             item.save(callback)
-        }, function (err, result) {
-            console.log(result, 'saved');
+        }, function () {
+            console.log('saved');
+            typeof cb == 'function' && cb()
         });
     }
 };
-
-
 
 
 //Exports
 module.exports = new DatabaseDriver();
-
