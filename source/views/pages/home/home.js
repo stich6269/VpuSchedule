6690120260c3523c90431f3936bdd76bc96f8004
stich6269 <stich6269@mail.ru>
2016-05-08 11:07:13 +0300

    - end development list of lessonf for a week

--- source/views/pages/home/home.js
+++ source/views/pages/home/home.js
@@ -1,10 +1,22 @@
-RAD.view("view.home", RAD.Blanks.View.extend({
+RAD.view("view.home", RAD.Blanks.ScrollableView.extend({
     url: 'source/views/pages/home/home.html',
     model: RAD.models.Lessons,
     currentDay: null,
     group: null,
+    events: {
+        'click .lesson' : 'onLessons'
+    },
     onNewExtras: function (data) {
         this.group = data.group;
+    },
+    onEndRender: function () {
+        console.log('end rendfer');
+    },
+    onLessons: function (e) {
+        var $item = $(e.currentTarget),
+            dayId = +$item.attr('data-target');
+
+        this.getCurrentDay(dayId);
     },
     onStartAttach: function () {
         var self = this;
@@ -13,7 +25,6 @@ RAD.view("view.home", RAD.Blanks.View.extend({
             success: function (resp) {
                 self.model.reset(resp, {silent: true});
                 self.getCurrentDay(2);
-                self.render();
             },
             error: function (err) {
                 console.log(err);
@@ -22,8 +33,14 @@ RAD.view("view.home", RAD.Blanks.View.extend({
     },
     getCurrentDay: function (dayId) {
         var data = this.model.toJSON();
+
         this.currentDay = _.filter(data, function(item){
             return item.date.dayIndex == dayId
-        })
+        });
+
+        this.render(function () {
+            $('[data-target=' + dayId +']').addClass('active');
+        });
+
     }
 }));
